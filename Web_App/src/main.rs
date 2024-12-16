1241d178060a334f9bedad45c34639a8cf669b29
Vinod Akshat <pumkininriver@gmail.com>
2024-12-16 23:59:55 +0530

    servinf html

--- Web_App/src/main.rs
+++ Web_App/src/main.rs
@@ -1,8 +1,12 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-
+use std::fs;
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
+}
+#[get("/application")]
+async fn application() -> impl Responder {
+    HttpResponse::Ok().body("Hello world from inside the app!")
 }
 
 #[post("/echo")]
@@ -13,12 +17,29 @@ async fn echo(req_body: String) -> impl Responder {
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
+async fn serve_html() -> impl Responder {
+    let html_content = fs::read_to_string("static/index.html")
+        .unwrap_or_else(|_| "Error loading index.html".to_string());
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(html_content)
+}
+async fn index() -> impl Responder {
+    "Html code !"
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
+            .service(application)
+            .service(
+                web::scope("/app")
+                    .route("/index.html", web::get().to(index)),
+            )
+            .route("/home", web::get().to(serve_html))
             .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
